b44fcb0f9cc67228d016cd21d81b48c4a4d9aed0
sausupo <antonn818@gmail.com>
2024-06-21 00:15:44 +0500

    add setStore

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WebApp from '@twa-dev/sdk'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const count = WebApp.CloudStorage.getItem("count") as unknown as string;
+
+  const click = () => {
+    const sum = Number(count) + 1;
+
+    WebApp.CloudStorage.setItem("count", String(sum));
+  }
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
+      </div> */}
       <h1>{`Привет ${WebApp.initDataUnsafe?.user?.last_name} ${WebApp.initDataUnsafe?.user?.first_name}`}</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={click}>
           count is {count}
         </button>
       </div>
