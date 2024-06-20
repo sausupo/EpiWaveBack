a8b69afb23a7bc6080072b23d9b162fa896b6ca0
sausupo <antonn818@gmail.com>
2024-06-21 00:00:46 +0500

    fix App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WebApp from '@twa-dev/sdk'
-import { MainButton } from '@twa-dev/sdk/react'
 
 function App() {
   const [count, setCount] = useState(0)
