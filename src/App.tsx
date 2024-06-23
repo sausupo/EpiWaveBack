2725cc8d0a3fb68006175829141db89c71863b5d
sausupo <antonn818@gmail.com>
2024-06-24 01:45:07 +0500

    add haptic test

--- src/App.tsx
+++ src/App.tsx
@@ -14,25 +14,6 @@ function App() {
     init(WebApp.initDataUnsafe.user ?? ({} as any));
   }, []);
 
-  // useEffect(() => {
-  //   WebApp.CloudStorage?.getItem("count", (error, result) => {setCount(error ? 0 : Number(result)); console.log(result)});
-  // }, []);
-
-  // useEffect(() => {
-  //   const delayDebounceFn = setTimeout(() => {
-  //     WebApp.CloudStorage.setItem("count", String(count), (error, result) => WebApp.showAlert(`aboba ${error} ${result}`));
-  //   }, 500)
-
-  //   return () => clearTimeout(delayDebounceFn)
-  // }, [count])
-
-  // const click = () => {
-  //   const sum = count + 1;
-
-  //   setCount(sum);
-  //   WebApp.CloudStorage.setItem("count", String(sum), (error, result) => WebApp.showAlert(`aboba ${error} ${result}`));
-  // }
-
   return (
     <>
       <div className="filter"/>
@@ -40,27 +21,6 @@ function App() {
         <source src={bg} type='video/mp4' />
       </video>
       <Header />
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
-      {/* <h1>{`Привет ${WebApp.initDataUnsafe?.user?.last_name} ${WebApp.initDataUnsafe?.user?.first_name}`}</h1>
-      <div className="card">
-        <button onClick={click}>
-          count is {count || 0}
-        </button>
-      </div> */}
-      {/* Here we add our button with alert callback */}
-      {/* <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-            Show Alert
-        </button>
-        <div>sfsafsfs {WebApp.initDataUnsafe?.user?.username}</div>
-      </div> */}
       <TgAppRoutes />
       <BottomNavigation />
       {/* <div id="viewport"></div>
--- src/pages/Home/ui/Home.tsx
+++ src/pages/Home/ui/Home.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./Home.css";
 import coin from "../../../assets/coin.png"
+import WebApp from "@twa-dev/sdk";
 
 export default function Home(): JSX.Element {
   const [count, setCount] = useState(5000);
 
   const click = () => {
+    WebApp.HapticFeedback.impactOccurred("light");
     setCount(count + 1);
   };
 
@@ -14,10 +16,16 @@ export default function Home(): JSX.Element {
       <div className="home-page__balance">
         <img src={coin} alt="coin" className="home-page__coin"/>
         <div className="home-page__count">{count}</div>
+        
       </div>
-      <div className="home-page__clicker" onTouchStart={click}>
+      <button style={{height: "100px", width: "100px"}} onClick={() => WebApp.HapticFeedback.impactOccurred("light")}>light</button>
+        <button style={{height: "100px", width: "100px"}} onClick={() => WebApp.HapticFeedback.impactOccurred("heavy")}>heavy</button>
+        <button style={{height: "100px", width: "100px"}} onClick={() => WebApp.HapticFeedback.impactOccurred("medium")}>medium</button>
+        <button style={{height: "100px", width: "100px"}} onClick={() => WebApp.HapticFeedback.impactOccurred("rigid")}>rigid</button>
+        <button style={{height: "100px", width: "100px"}} onClick={() => WebApp.HapticFeedback.impactOccurred("soft")}>soft</button>
+      {/* <div className="home-page__clicker" onTouchStart={click}> */}
       {/* <img src={clicker} alt="coin"/> */}
-      </div>
+      {/* </div> */}
     </div>
   );
 }
