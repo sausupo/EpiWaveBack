ebe5f104398a7ddab32c341637c64236c5e2a109
sausupo <antonn818@gmail.com>
2024-06-25 11:56:22 +0500

    add animation for click

--- src/pages/Home/ui/Home.tsx
+++ src/pages/Home/ui/Home.tsx
@@ -1,17 +1,10 @@
-import { useState } from "react";
 import "./Home.css";
 import coin from "../../../assets/coin.png"
-import WebApp from "@twa-dev/sdk";
-// import clicker from "../../../../public/clickerv2.png";
-import clicker from "../../../assets/clicker.png";
+import { Clicker } from "../../../widgets/Clicker";
+import useClicker from "../../../store/useClicker";
 
 export default function Home(): JSX.Element {
-  const [count, setCount] = useState(5000);
-
-  const click = () => {
-    WebApp.HapticFeedback.impactOccurred("light");
-    setCount(count + 1);
-  };
+  const count = useClicker(state => state.count)
 
   return (
     <div className="home-page">
@@ -19,11 +12,7 @@ export default function Home(): JSX.Element {
         <img src={coin} alt="coin" className="home-page__coin"/>
         <div className="home-page__count">{count}</div>
       </div>
-      <div className="home-page__clicker" onTouchStart={click}>
-        <div className="glass">
-          <img src={clicker} className="home-page__ff" alt="coin"/>
-        </div>
-      </div>
+      <Clicker/>
     </div>
   );
 }
