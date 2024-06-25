2d76c61c31506bf298059db4bcf9156069a13a1c
sausupo <antonn818@gmail.com>
2024-06-25 14:47:41 +0500

    add format for count

--- src/pages/Home/ui/Home.tsx
+++ src/pages/Home/ui/Home.tsx
@@ -3,6 +3,13 @@ import coin from "../../../assets/coin.svg"
 import { Clicker } from "../../../widgets/Clicker";
 import useClicker from "../../../store/useClicker";
 
+function formatNumberWithoutCurrency(value: number, locale: string = 'en-US', minFractionDigits: number = 0, maxFractionDigits: number = 0): string {
+  return value.toLocaleString(locale, {
+      minimumFractionDigits: minFractionDigits,
+      maximumFractionDigits: maxFractionDigits
+  });
+}
+
 export default function Home(): JSX.Element {
   const count = useClicker(state => state.count)
 
@@ -10,7 +17,7 @@ export default function Home(): JSX.Element {
     <div className="home-page">
       <div className="home-page__balance">
         <img src={coin} alt="coin" className="home-page__coin"/>
-        <div className="home-page__count">{count}</div>
+        <div className="home-page__count">{formatNumberWithoutCurrency(count, 'en-US')}</div>
       </div>
       <Clicker/>
     </div>
